81b164a0818e88e4ca0b0094388c3fe35c6abcb1
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-07-09 14:12:09 +0300

    Implement TokenService and InvalidTokenService in auth

---
+++ src/main/java/com/springboot/springbootsecurity/auth/service/InvalidTokenService.java
@@ -0,0 +1,10 @@
+package com.springboot.springbootsecurity.auth.service;
+
+import java.util.Set;
+
+public interface InvalidTokenService {
+
+    void invalidateTokens(final Set<String> tokenIds);
+
+    void checkForInvalidityOfToken(final String tokenId);
+}
