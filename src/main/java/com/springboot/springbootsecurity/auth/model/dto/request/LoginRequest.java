4ea827194feec22907b345455d40aa9da2c366b7
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2024-07-09 14:23:32 +0300

    Revise LoginRequest

--- src/main/java/com/springboot/springbootsecurity/auth/model/dto/request/LoginRequest.java
+++ src/main/java/com/springboot/springbootsecurity/auth/model/dto/request/LoginRequest.java
@@ -4,9 +4,11 @@ package com.springboot.springbootsecurity.auth.model.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Builder;
 
 @Getter
 @Setter
+@Builder
 public class LoginRequest {
 
     @NotBlank
